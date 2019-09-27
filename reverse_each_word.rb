39cd43d0d94f9665c826e334e41462947c11fbdf
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 01:18:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   result = []
   sentence = str.split(" ")
-  sentence.each do |word|
+  sentence.collect do |word|
     result.push(word.reverse)
   end
   result.join(" ")
